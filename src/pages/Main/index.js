24027085904941e26e5e52ef1c3ba1b6c77047fb
Felipe Lucena <76856707+felipelucena098@users.noreply.github.com>
2023-03-01 21:04:31 -0300

    feat: criar state para salvar nome do usuário

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { MdSearch } from 'react-icons/md';
 import * as S from './styles';
 
 import LogoApiGitHub from '../../assets/images/github-logo.svg';
 
-function Main() {
+const Main = () => {
+  const [login, setLogin] = useState('');
   return (
     <S.Container>
       <S.Logo src={LogoApiGitHub} alt="Logo GitHub" />
       <S.Title>API Github</S.Title>
       <S.Form>
-        <S.Input placeholder="usuário" />
+        <S.Input
+          placeholder="usuário"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        />
         <S.Button>
           <MdSearch size={42} />
         </S.Button>
       </S.Form>
     </S.Container>
   );
-}
+};
 
 export default Main;
